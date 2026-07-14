a85dbcfa82142f48e54ebacebe4173f3f0d171c1
Scotty <papamoascotty@gmail.com>
2026-07-14 22:29:53 +1200

    Initial release: Ecocore Boilerplate

---
+++ main/90_Studio_Export.c
@@ -0,0 +1,84 @@
+#include "90_Studio_Export.h"
+#include "lvgl.h"
+#include "20_RTC.h"
+#include "30_WIFI.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+static lv_obj_t * fg_clock_label = NULL;
+static lv_obj_t * fg_wifi_label = NULL;
+
+static void fg_clock_tick_cb(lv_timer_t *timer)
+{
+    LV_UNUSED(timer);
+
+    static bool show_colon = true;
+
+    char time_buf[16];
+    fg_rtc_format_time(time_buf, sizeof(time_buf));
+
+    if (!show_colon)
+    {
+        time_buf[2] = ' ';
+    }
+
+    show_colon = !show_colon;
+
+    if (fg_clock_label)
+    {
+        lv_label_set_text(fg_clock_label, time_buf);
+    }
+}
+
+static void fg_wifi_tick_cb(lv_timer_t *timer)
+{
+    LV_UNUSED(timer);
+
+    if (!fg_wifi_label)
+    {
+        return;
+    }
+
+    fg_wifi_pump();
+
+    char wifi_buf[128];
+    snprintf(wifi_buf, sizeof(wifi_buf), "WIFI\n%s\nIP: %s", fg_wifi_status_text(), fg_wifi_ip_text());
+    lv_label_set_text(fg_wifi_label, wifi_buf);
+}
+
+// ForgeUI LVGL Export Proof V1
+// Generated from ForgeUI Studio
+
+void fg_studio_export_create(lv_obj_t *parent)
+{
+    // Background flavour: Military Plate
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x1B2416), 0);
+    lv_obj_set_style_bg_opa(lv_screen_active(), LV_OPA_COVER, 0);
+    lv_obj_set_style_bg_color(parent, lv_color_hex(0x1B2416), 0);
+    lv_obj_set_style_bg_opa(parent, LV_OPA_COVER, 0);
+
+    LV_IMAGE_DECLARE(fg_upload_industrial_panel_8775311d);
+    lv_obj_t * bg_texture_0 = lv_image_create(parent);
+    lv_image_set_src(bg_texture_0, &fg_upload_industrial_panel_8775311d);
+    lv_obj_set_pos(bg_texture_0, 0, 0);
+    lv_obj_t * bg_texture_1 = lv_image_create(parent);
+    lv_image_set_src(bg_texture_1, &fg_upload_industrial_panel_8775311d);
+    lv_obj_set_pos(bg_texture_1, 512, 0);
+    lv_obj_t * bg_texture_2 = lv_image_create(parent);
+    lv_image_set_src(bg_texture_2, &fg_upload_industrial_panel_8775311d);
+    lv_obj_set_pos(bg_texture_2, 0, 512);
+    lv_obj_t * bg_texture_3 = lv_image_create(parent);
+    lv_image_set_src(bg_texture_3, &fg_upload_industrial_panel_8775311d);
+    lv_obj_set_pos(bg_texture_3, 512, 512);
+    lv_obj_move_background(bg_texture_0);
+    lv_obj_move_background(bg_texture_1);
+    lv_obj_move_background(bg_texture_2);
+    lv_obj_move_background(bg_texture_3);
+
+
+    fg_clock_tick_cb(NULL);
+    lv_timer_create(fg_clock_tick_cb, 1000, NULL);
+
+    fg_wifi_tick_cb(NULL);
+    lv_timer_create(fg_wifi_tick_cb, 1000, NULL);
+}
